e9436fb76fde959e544e2e878b55602447ec0e7c
sudhakars10 <sudhakars10@gmail.com>
2024-09-05 10:44:43 +0100

    Feat: Adding the BasePage.java to instantiate the driver and wait objects

---
+++ src/test/java/com/zalando/pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.zalando.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+}
