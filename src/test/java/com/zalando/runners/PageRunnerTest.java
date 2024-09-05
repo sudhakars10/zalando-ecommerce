eda464e87b3464c8f89a36667844d89a328cd983
sudhakars10 <sudhakars10@gmail.com>
2024-09-05 18:04:15 +0100

    Feat: Changing the tag typo to fix the issue

--- src/test/java/com/zalando/runners/PageRunnerTest.java
+++ src/test/java/com/zalando/runners/PageRunnerTest.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(tags = "@Smoke", features = {"src/test/resources/features"}, glue = {"com.zalando"},
+@CucumberOptions(tags = "@smoke", features = {"src/test/resources/features"}, glue = {"com.zalando"},
         plugin = {"pretty","html:target/cucumber/cucumber.html"})
 public class PageRunnerTest extends AbstractTestNGCucumberTests{
     @Override
