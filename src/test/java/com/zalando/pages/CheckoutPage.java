20053d39f975b86f18d8475aae073d7041da6ff3
sudhakars10 <sudhakars10@gmail.com>
2024-09-05 11:17:12 +0100

    Feat: Adding the files for checkout page automation - TODO

---
+++ src/test/java/com/zalando/pages/CheckoutPage.java
@@ -0,0 +1,11 @@
+package com.zalando.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage extends BasePage {
+    public CheckoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    //TODO
+}
---
+++ src/test/java/com/zalando/stepdefinitions/CheckoutStepDefinitions.java
@@ -0,0 +1,5 @@
+package com.zalando.stepdefinitions;
+
+public class CheckoutStepDefinitions {
+    //TODO
+}
